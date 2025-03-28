7485300de27a408f2292c2db2eebcbac756d7332
V N Rajesh Kumar Kaki <RajeshKaki@users.noreply.github.com>
2025-03-28 13:30:46 +0530

    created project for resiliency retry and timeout strategy

---
+++ Resilency-1/ClientApp/ApiClient.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace ClientApp
+{
+    public class APIClient
+    {
+        private HttpClient _client;
+        public APIClient(IHttpClientFactory httpClientfactory)
+        {
+            _client = httpClientfactory.CreateClient("RetryHandler");
+
+            Console.WriteLine(_client.BaseAddress);
+        }
+        public void InvokeApi()
+        {
+            Stopwatch w = Stopwatch.StartNew();
+            try
+            {
+                w.Start();
+
+                var response = _client.GetAsync("http://localhost:5171/WeatherForecast").Result;
+                Console.WriteLine(response.IsSuccessStatusCode);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                w.Stop();
+                Console.WriteLine($"Total Time taken: {w.ElapsedMilliseconds}");
+                w.Restart();
+            }
+        }
+
+    }
+}
---
+++ Resilency-1/ClientApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http.Resilience;
+using Polly;
+using Microsoft.Extensions.Logging;
+using Polly.Telemetry;
+namespace ClientApp
+{
+    internal class Program
+    {
+        public class DummyClass
+        {
+            public int Value { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            ServiceCollection services = new ServiceCollection();
+            using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+            services.AddHttpClient("RetryHandler")
+                    .AddResilienceHandler("RetryHander1",
+                    (resilienceBuilder) =>
+                    {
+                        resilienceBuilder.ConfigureTelemetry(factory);
+
+                        // Retry Strategy configuration
+                        resilienceBuilder.AddRetry(new HttpRetryStrategyOptions // Configures retry behavior
+                        {
+                            MaxRetryAttempts = 2, // Maximum retries before throwing an exception (default: 3)
+
+                            Delay = TimeSpan.FromSeconds(2), // Delay between retries (default: varies by strategy)
+
+                            BackoffType = DelayBackoffType.Linear, // Exponential backoff for increasing delays (default)
+
+                            UseJitter = false, // Adds random jitter to delay for better distribution (default: false)
+
+                            ShouldHandle = new PredicateBuilder<HttpResponseMessage>() // Defines exceptions to trigger retries
+                            .Handle<Exception>()
+                            .Handle<HttpRequestException>()
+                            .HandleResult(response => !(response.IsSuccessStatusCode) && response.StatusCode >= System.Net.HttpStatusCode.InternalServerError)
+                            // Includes non-successful responses
+                        });
+
+                        // Timeout Strategy configuration
+                        resilienceBuilder.AddTimeout(TimeSpan.FromSeconds(10)); // Sets a timeout limit for requests (throws TimeoutRejectedException)
+
+                    }
+                   );
+
+            services.AddSingleton<APIClient>();
+            var service = services.BuildServiceProvider();
+            var api = service.GetRequiredService<APIClient>();
+
+            api.InvokeApi();
+
+        }
+    }
+}
